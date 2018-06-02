a6a2ce1fd0d4cca26b51946ecda117d3c72250dd
Puneet Kashyap <pkashyaps@gmail.com>
2018-06-02 12:38:31 -0400

    fixed footer to use bootstrap only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,29 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LocationComponent } from './location/location.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LocationComponent
+    LocationComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
