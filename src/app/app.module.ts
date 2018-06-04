b4a63bde57d38fe64c33876687c61129b3858c2d
Puneet Kashyap <pkashyaps@gmail.com>
2018-06-04 12:49:45 -0400

    routes added and few more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,6 +14,28 @@ import { FooterComponent } from './footer/footer.component';
 import { LocationComponent } from './location/location.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: "Sahar's Kitchen"}
+  },
+  {
+    path: 'location',
+    component: LocationComponent,
+    data: { title: "Sahar's Kitchen Location"}
+  },
+  {
+    path: 'reservations',
+    component: ReservationComponent,
+    data: { title: "Sahar's Kitchen Reservation"}
+  },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact',component: ContactsComponent }
+]
 
 @NgModule({
   declarations: [
@@ -21,7 +44,9 @@ import { MenuComponent } from './menu/menu.component';
     FooterComponent,
     LocationComponent,
     ContactsComponent,
-    MenuComponent
+    MenuComponent,
+    ReservationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +55,10 @@ import { MenuComponent } from './menu/menu.component';
     MatMenuModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
