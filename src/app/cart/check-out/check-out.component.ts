b8293f54ac253f197e3f0a5f587692359902b9c0
Puneet Kashyap <pkashyaps@gmail.com>
2018-10-18 15:41:32 -0400

    order connected to database

--- src/app/cart/check-out/check-out.component.ts
+++ src/app/cart/check-out/check-out.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { FirebaseService } from '../../firebase.service';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { CartService } from "../../cart.service";
+import { FirebaseService } from "../../firebase.service";
 
 @Component({
-  selector: 'app-check-out',
-  templateUrl: './check-out.component.html',
-  styleUrls: ['./check-out.component.css']
+  selector: "app-check-out",
+  templateUrl: "./check-out.component.html",
+  styleUrls: ["./check-out.component.css"]
 })
 export class CheckOutComponent implements OnInit {
-  times = ['11:30 am', '12:00 pm', '01:00 pm', '01:30 pm', '02:00 pm', '02:30 pm',
-  '05:00 pm', '06:00 pm', '07:00 pm', '08:00 pm', '09:00 pm', '10:00 pm'];
-  
-  constructor(private firebaseService: FirebaseService) { }
+  times = [
+    "11:30 am",
+    "12:00 pm",
+    "01:00 pm",
+    "01:30 pm",
+    "02:00 pm",
+    "02:30 pm",
+    "05:00 pm",
+    "06:00 pm",
+    "07:00 pm",
+    "08:00 pm",
+    "09:00 pm",
+    "10:00 pm"
+  ];
+  cartOrders;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
-    window.scroll(0,0);
+    window.scroll(0, 0);
   }
 
-  onSubmit(form:NgForm){
-    console.log('Here is the complete:' + form);
-    this.firebaseService.writeToDatabase('orders', form);
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.cartOrders = this.cartService.cartOrders;
+      console.log(this.cartOrders)
+      this.router.navigate(["/orders"]);
+      this.firebaseService.writeToDatabase("orders", {...form.value,...this.cartOrders});
+    }
   }
-
 }
