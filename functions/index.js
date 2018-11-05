babb98a85f8d9c91c4d5d5f9f2f8c923fb36793f
Puneet Kashyap <pkashyaps@gmail.com>
2018-11-05 15:11:40 -0500

    Email functionality added when new order received in DB

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,28 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const sendEmail = require("./email");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const email = require('./email');
 
 admin.initializeApp(functions.config().firebase);
 
 const recipientEmailIDs = [
-    functions.config().email.recipient1,
-    functions.config().email.recipient2
-]
+  functions.config().email.recipient1,
+  functions.config().email.recipient2
+];
 
 exports.sendReservationsInfo = functions.firestore
-  .document("/Reservations/{order}")
+  .document('/Reservations/{order}')
   .onCreate((snap, context) => {
-    console.log("New Entry added");
+    console.log('Reservation Entry added');
     const original = snap.data();
-    console.log("Values", original);
-    sendEmail(recipientEmailIDs, original);
+    console.log('Values', original);
+    email.sendReservationEmail(recipientEmailIDs, original);
+  });
+
+exports.sendOrderInfo = functions.firestore
+  .document('/Orders/{order}')
+  .onCreate((snap, context) => {
+    console.log('Order Entry added');
+    const original = snap.data();
+    console.log('Values', original);
+    email.sendOrderEmail(functions.config().email.id, original);
   });
